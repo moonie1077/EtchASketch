fe6c1f01ffd66ef844725dfda8382e51b54e8451
g145773 <andrew.j.laguana@kp.org>
2023-03-08 07:27:07 -0800

    Pixel grid completed and sized accurately

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container");
+
+
+for (let i= 0; i<16; i++) {
+  const row = document.createElement("div");
+  row.classList.add('row');
+  container.appendChild(row)
+
+  for (let j = 0; j<16; j++)
+      {
+      const pixel = document.createElement("div");
+      pixel.classList.add('pixel');
+      row.appendChild(pixel);
+      }
+  }
